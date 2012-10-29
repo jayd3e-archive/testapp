706bbb3eb8af3b282924c16951f9d33a3720d5f6
Joe Dallago <jd.dallago@gmail.com>
2012-10-29 10:20:56 -0500

    Added SQLAlchemy support for testapp.

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,9 +1,24 @@
 from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+from sqlalchemy.orm import sessionmaker
+from testapp.models import initialize_base
+
+
+def get_db(request):
+    maker = request.registry.settings['db.sessionmaker']
+    return maker()
 
 
 def main(global_config, **settings):
     '''Main config function'''
+
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    initialize_base(engine)
+    maker = sessionmaker(bind=engine)
+    settings['db.sessionmaker'] = maker
+
     config = Configurator(settings=settings)
+    config.set_request_property(get_db, 'db', reify=True)
 
     # Routes
     config.add_route('index', '/')
--- testapp/templates/posts_index.mako
+++ testapp/templates/posts_index.mako
@@ -6,10 +6,10 @@
         <div class="posts">
             % for post in posts:
                 <div class="post">
-                    <h2>${ post['name'] }</h2>
-                    <span>${ post['id'] }</span>
-                    <div>${ post['description'] }</div>
-                    <div>${ post['date'] }</div>
+                    <h2>${ post.name }</h2>
+                    <span>${ post.id }</span>
+                    <div>${ post.description }</div>
+                    <div>${ post.date }</div>
                 </div>
             % endfor
         </div>
